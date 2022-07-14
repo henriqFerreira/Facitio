290df61c6f74a71ce6a175b90174b05addf6c85a
Henrique Neto <henriqueneto1912@gmail.com>
2022-07-14 17:01:44 -0300

    new: Adicionando parte 1 da página sign-in/sign-up

---
+++ protected/view/sign-in.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Facitio: Qual será o seu perfil?</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../css/module/auth/auth.css">
+</head>
+<body>
+    <section id="auth-wrapper">
+        <div class="auth-p1">
+            <h3>Qual será o seu perfil?</h3>
+            <a href="#" class="lg-btn">Quero ser um profissional</a>
+            <a href="#" class="lg-btn">Quero ser um cliente</a>
+        </div>
+    </section>
+</body>
+</html>
