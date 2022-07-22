d9ef02fa672723bd0c8f2f762f474b4f689d9401
Henrique Neto <henriqueneto1912@gmail.com>
2022-07-22 16:27:11 -0300

    update: Continução da página sign-in/sign-up

---
+++ protected/view/auth/sign-in-c.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Facitio: Cliente</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../../../img/favicon.svg" type="image/x-icon">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../../css/module/auth/auth.css">
+</head>
+<body>
+    <section id="auth-wrapper">
+        <form action="#">
+            <h3>Crie sua conta como cliente</h3>
+            <div class="progress-bar">
+                <span class="step"></span>
+                <span class="step"></span>
+                <span class="step"></span>
+                <span class="step"></span>
+            </div>
+            <div class="form-step active-step">
+                <div class="input-group">
+                    <label for="inp-nome">Nome</label>
+                    <input type="text" name="inp-nome">
+                </div>
+                <div class="input-group">
+                    <label for="inp-sobrenome">Sobrenome</label>
+                    <input type="text" name="inp-sobrenome">
+                </div>
+                <a class="btn btn-seguinte">Seguinte</a>
+            </div>
+            <div class="form-step active-step"></div>
+            <div class="form-step active-step"></div>
+            <div class="form-step active-step"></div>
+        </form>
+    </section>
+</body>
+</html>
--- protected/view/auth/sign-in-profile.php
+++ protected/view/auth/sign-in-profile.php
@@ -5,15 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Facitio: Qual será o seu perfil?</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../../../img/favicon.svg" type="image/x-icon">
     <!-- CSS -->
-    <link rel="stylesheet" href="../../css/module/auth/auth.css">
+    <link rel="stylesheet" href="../../../css/module/auth/auth.css">
 </head>
 <body>
     <section id="auth-wrapper">
-        <div class="auth-p1">
+        <div class="auth-profile">
             <h3>Qual será o seu perfil?</h3>
-            <a href="#" class="lg-btn">Quero ser um profissional</a>
-            <a href="#" class="lg-btn">Quero ser um cliente</a>
+            <a href="sign-in-p.php" class="lg-btn">Quero ser um profissional</a>
+            <a href="sign-in-c.php" class="lg-btn">Quero ser um cliente</a>
         </div>
     </section>
 </body>
