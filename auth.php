6b1d757a1fc765c2859b01d854466b28615936bf
Gustavo Piovani <gus.piovani@gmail.com>
2022-10-24 13:45:54 -0300

    auth-page

---
+++ auth.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Facitio: Soluções fáceis para seus problemas difíceis</title>
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> 
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="./img/favicon.svg" type="image/x-icon">
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/module/auth/auth.css">
+</head>
+<body>
+    <main id="auth-wrapper">
+        <div>
+
+        </div>
+        <div>
+
+        </div>      
+    </main>
+</body>
+</html>
