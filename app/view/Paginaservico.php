bf8dbda3a067de5a809c397fba0556826d4dddaa
Gustavo Piovani <gus.piovani@gmail.com>
2022-11-09 23:23:30 -0200

    Estilizando ícones lista não ordenada
    Coloquei um Icon de casa só pra testar, depois é só colocar o joinha/check

--- app/view/Paginaservico.php
+++ app/view/Paginaservico.php
@@ -73,6 +73,8 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/Facitio/app/core/Config.php';
                                         <li>3</li>
                                         <li>4</li>
                                         <li>5</li>
+                                        <li>6</li>
+                                        <li>7</li>
                                     </ul>
                                 </div>
                                 <div >
