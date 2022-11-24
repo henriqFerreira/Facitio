f8ac4f263ef0d8a8d8d6054954013dfb8eda7666
Henrique Neto <henriquepfn@gmail.com>
2022-11-23 22:27:58 -0200

    Profile update

--- app/controller/Profile.php
+++ app/controller/Profile.php
@@ -24,9 +24,10 @@ class Profile extends Controller {
     }
 
     function updateProfile($updatingData, $userType) {
-        $dom = new DOMDocument();
         $database = new Database();
         $errorHandler = new Errors();
+        $userId = $userType."_id";
+        unset($updatingData['submit']);
 
         if (!$errorHandler->isFileInputValid($_FILES))
             $profileImageContent = $_SESSION['logged']['Foto'];
@@ -34,16 +35,11 @@ class Profile extends Controller {
             $profileImageContent = file_get_contents($_FILES['profileImage']['tmp_name']);
 
         foreach ($updatingData as $key => $value) {
-            if (empty($updatingData[$key])) {
+            if (empty($updatingData[$key]))
                 $updatingData[$key] = $_SESSION['logged'][ucfirst($key)];
-            }
         }
 
-        echo "<pre>";
-        print_r($updatingData);
-        echo "</pre>";
-
-        $params = array(
+        $tb_login = array(
             'nome' => $updatingData['nome'],
             'sobrenome' => $updatingData['sobrenome'],
             'email' => $updatingData['email'],
@@ -51,21 +47,48 @@ class Profile extends Controller {
             'datanasc' => $updatingData['datanasc'],
             'contato' => $updatingData['contato'],
             'foto' => $profileImageContent,
+            'cpf' => $_SESSION['logged']['Cpf']
+        );
+
+        $tb_endereco = array(
             'rua' => $updatingData['rua'],
             'num' => $updatingData['num'],
             'complemento' => $updatingData['complemento'],
             'bairro' => $updatingData['bairro'],
             'cidade' => $updatingData['cidade'],
             'estado' => $updatingData['estado'],
-            'cep' => $updatingData['cep'],
+            'cep' => $updatingData['cep']
         );
 
-        $userQuery = "UPDATE TABLE tb_login_{$userType} SET {$userType}_nome = :nome,
+        echo '<pre>';
+        print_r($tb_endereco);
+        echo '</pre>';
+
+        echo '<pre>';
+        print_r($tb_login);
+        echo '</pre>';
+
+        $find = "SELECT {$userType}_id FROM tb_login_{$userType} WHERE {$userType}_cpf = :cpf";
+        $id = $database->read($find, ['cpf' => $_SESSION['logged']['Cpf']])[0]->$userId;
+
+        $userQuery = "UPDATE tb_login_{$userType} SET {$userType}_nome = :nome,
                                                             {$userType}_sobrenome = :sobrenome,
                                                             {$userType}_email = :email,
                                                             {$userType}_rg = :rg,
                                                             {$userType}_datanasc = :datanasc,
                                                             {$userType}_contato = :contato,
                                                             {$userType}_foto = :foto WHERE {$userType}_cpf = :cpf";
+        $database->write($userQuery, $tb_login);
+
+        $enderecoQuery = "UPDATE tb_endereco_{$userType} SET end_nome = :rua,
+                                                                   end_num = :num,
+                                                                   end_complemento = :complemento,
+                                                                   end_bairro = :bairro,
+                                                                   end_cidade = :cidade,
+                                                                   end_estado = :estado,
+                                                                   end_cep = :cep WHERE {$userType}_id = {$id}";
+        $database->write($enderecoQuery, $tb_endereco);
+
+        $_SESSION['logged']['Nome']
     }
 }
